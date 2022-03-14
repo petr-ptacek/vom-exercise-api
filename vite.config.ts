e6539742bcb540f6b85aca2fffdb011c753ed845
Petr Ptacek <petr.ptacek@olc.cz>
2022-03-14 08:31:25 +0100

    updates

--- vite.config.ts
+++ vite.config.ts
@@ -6,14 +6,9 @@ export default defineConfig({
     sourcemap: process.env.NODE_ENV === 'development' ? 'inline' : false,
     lib: {
       name: 'ExerciseApi',
-      formats: ['umd', 'es'],
+      formats: ['es'],
       entry: path.resolve(__dirname, './src/index.ts'),
       fileName: (format => `vom-exercise-api.${ format }.js`)
-    }
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
     }
   }
 });
